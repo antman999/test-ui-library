9dfbeae81c73a03fcbf7045e04f445dc834758a3
antman999 <anthonymendoza9922@gmail.com>
2023-06-06 11:48:54 -0400

    configured the library

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,9 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    sidebar: {
+      collapsedRoots: [],
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
